2fb8786a791096df4a790d83311a2c86553b080e
joaopedrodcf <joaoferr93@gmail.com>
2018-04-29 00:48:03 +0100

    test

--- App.js
+++ App.js
@@ -2,23 +2,25 @@ import React from 'react';
 import { Container, Header, Left, Body, Title, Right, Content, Textarea, Form } from 'native-base';
 import { AppFontLoader } from './utils/AppFontLoader';
 
-const App = () => (
-  <AppFontLoader>
-    <Container>
-      <Header>
-        <Left />
-        <Body>
-          <Title>Textarea</Title>
-        </Body>
-        <Right />
-      </Header>
-      <Content padder>
-        <Form>
-          <Textarea rowSpan={5} bordered placeholder="Textarea" />
-        </Form>
-      </Content>
-    </Container>
-  </AppFontLoader>
-);
-
-export default App;
+export default class App extends React.Component {
+  render() {
+    return (
+      <AppFontLoader>
+        <Container>
+          <Header>
+            <Left />
+            <Body>
+              <Title>Textarea</Title>
+            </Body>
+            <Right />
+          </Header>
+          <Content padder>
+            <Form>
+              <Textarea rowSpan={5} bordered placeholder="Textarea" />
+            </Form>
+          </Content>
+        </Container>
+      </AppFontLoader>
+    );
+  }
+}
