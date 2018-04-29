e695fa356e5e98e42a0cc83940cd6c7e8497f8c1
joaopedrodcf <joaoferr93@gmail.com>
2018-04-29 01:55:43 +0100

    CRLF to LF

--- App.js
+++ App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import { Container, Header, Left, Body, Title, Right, Content, Textarea, Form } from 'native-base';
-import { AppFontLoader } from './utils/AppFontLoader';
+import { StyleSheet, View } from 'react-native';
+import { FormLabel, Button, Header } from 'react-native-elements';
+import Textarea from 'react-native-textarea';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textareaContainer: {
+    height: 180,
+    padding: 5,
+    backgroundColor: '#F5FCFF',
+  },
+  textarea: {
+    textAlignVertical: 'top', // hack android
+    height: 170,
+    fontSize: 14,
+    color: '#333',
+  },
+});
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    };
+  }
+
   render() {
     return (
-      <AppFontLoader>
-        <Container>
-          <Header>
-            <Left />
-            <Body>
-              <Title>Textarea</Title>
-            </Body>
-            <Right />
-          </Header>
-          <Content padder>
-            <Form>
-              <Textarea rowSpan={5} bordered placeholder="Textarea" />
-            </Form>
-          </Content>
-        </Container>
-      </AppFontLoader>
+      <View style={styles.container}>
+        <Header
+          leftComponent={{ icon: 'menu', color: '#fff' }}
+          centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+          rightComponent={{ icon: 'home', color: '#fff' }}
+        />
+        <FormLabel>Your post</FormLabel>
+        <Textarea
+          containerStyle={styles.textareaContainer}
+          style={styles.textarea}
+          defaultValue={this.state.text}
+          maxLength={120}
+          placeholder="好玩有趣的，大家同乐，伤感忧闷的，大家同哭。。。"
+          placeholderTextColor="#c7c7c7"
+          underlineColorAndroid="transparent"
+          onChangeText={text => this.setState({ text })}
+        />
+        <Button
+          title="BUTTON"
+          style={{ marginTop: 20 }}
+          onPress={() => this.setState({ text: '' })}
+        />
+      </View>
     );
   }
 }
