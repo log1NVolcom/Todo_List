57228f1f0a5a1df5643946d594d3d9c6f0fa766b
joaopedrodcf <joaoferr93@gmail.com>
2018-05-01 02:15:57 +0100

    new design of the front page

--- README.md
+++ README.md
@@ -28,6 +28,10 @@ Now its ready to use
 
 You need to install Node.js
 
+### Sizes
+
+24px for header of google
+
 ## Built With
 
 * [npm](https://github.com/npm/npm) - Package manager for javascript
@@ -35,3 +39,4 @@ You need to install Node.js
 * [react-router](https://github.com/ReactTraining/react-router) - Declarative routing for React
 * [react-native](https://github.com/facebook/react-native) - A framework for building native apps with React.
 * [react-native-elements](https://github.com/react-native-training/react-native-elements) - Cross Platform React Native UI Toolkit
+* [coolors](https://coolors.co/) - Color picker
--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,22 +1,48 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { FormLabel, Button, Header } from 'react-native-elements';
-import Textarea from 'react-native-textarea';
+
+import { View, StyleSheet, Text, Button, TextInput } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#1b98e0',
   },
-  textareaContainer: {
-    height: '70%',
-    padding: 5,
-    backgroundColor: '#F5FCFF',
+  header: {
+    height: 80,
+    paddingTop: 24,
+    backgroundColor: '#13293d',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'center',
+    marginBottom: 20,
   },
-  textarea: {
+  headerText: {
+    color: '#e8f1f2',
+    fontSize: 20,
+  },
+  main: {
+    marginLeft: 10,
+    marginRight: 10,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    backgroundColor: '#e8f1f2',
+    height: 150,
+    marginTop: 0,
+    paddingTop: 0,
     textAlignVertical: 'top', // hack android
-    height: 170,
-    fontSize: 14,
-    color: '#333',
+    marginBottom: 20,
+    fontSize: 18,
+  },
+  textInputLabel: {
+    color: '#e8f1f2',
+    fontSize: 20,
+  },
+  button: {
+    borderRadius: 10,
+    marginTop: 20,
   },
 });
 
@@ -31,30 +57,30 @@ class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Header
-          leftComponent={{ icon: 'menu', color: '#fff' }}
-          centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
-          rightComponent={{ icon: 'home', color: '#fff' }}
-        />
-        <FormLabel>Your post</FormLabel>
-        <Textarea
-          containerStyle={styles.textareaContainer}
-          style={styles.textarea}
-          defaultValue={this.state.text}
-          maxLength={120}
-          placeholder="Enter Text"
-          placeholderTextColor="#c7c7c7"
-          underlineColorAndroid="transparent"
-          onChangeText={text => this.setState({ text })}
-        />
-        <Button
-          title="Clear Text"
-          buttonStyle={{
-            borderRadius: 10,
-            marginTop: 20,
-          }}
-          onPress={() => this.setState({ text: '' })}
-        />
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Todo List</Text>
+        </View>
+        <View style={styles.main}>
+          <Text style={styles.textInputLabel}>Create your post here:</Text>
+
+          <TextInput
+            style={styles.textInput}
+            multiline
+            maxLength={120}
+            underlineColorAndroid="transparent"
+            onChangeText={text => this.setState({ text })}
+            value={this.state.text}
+          />
+
+          <View style={styles.footer}>
+            <Button
+              title="Create post"
+              color="#3d4f60" // buttons cant use backgroundColor
+              style={styles.button}
+              onPress={() => this.setState({ text: '' })}
+            />
+          </View>
+        </View>
       </View>
     );
   }
